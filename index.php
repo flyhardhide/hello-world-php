c9e64bb4b8814df8074b94631cfc9dae2ca4c4d0
HS <57307666+flyhardhide@users.noreply.github.com>
2023-05-11 13:41:01 +0900

    Update index.php
    You IP display enabled.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <html><head><title>TEST PHP CODE</title></head>
   <body>
 <?php 
-print "Hello World";
+    print(date('Y/m/d'));
+    $ip=$_SERVER["REMOTE_ADDR"];
+print("Your IP is: ".$ip);
 ?>
   </body>
 </html>
