93f7ad16b030cb4b3c939452abf4412112f283f1
HS <57307666+flyhardhide@users.noreply.github.com>
2023-05-11 13:59:11 +0900

    Update index.php
    http_x-forwarded_forをテスト

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html><head><title>TEST PHP CODE</title></head>
   <body>
 <?php 
-    print(date('Y/m/d'));
-    $ip=$_SERVER["REMOTE_ADDR"];
+    print(date('Y/m/d')."<br />");
+    $ip=$_SERVER['REMOTE_ADDR'];
+    $ip2=$_SERVER['HTTP_X_FORWARDED_FOR'];
 print("Your IP is: ".$ip);
+    print("<br />Or : ".$ip2);
 ?>
   </body>
 </html>
